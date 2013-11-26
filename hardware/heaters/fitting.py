3484d1710087cee85fddb224a3754878a0429682
Pete Shadbolt <pete.shadbolt@gmail.com>
2013-11-26 15:10:55 +0000

    for alex

--- hardware/heaters/fitting.py
+++ hardware/heaters/fitting.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy.optimize import fmin
 
-# Fitting stuff
-#def phase(p, v): return p[1]+p[2]*(v**2)+p[3]*(v**3)+p[4]*(v**4)
 
-# first two parameters are for the phase-voltage relation.
-# second two parameters are for the sinusoidal fit.
+'''
+First two parameters are for the phase-voltage relation.
+Second two parameters are for the sinusoidal fit.
+'''
 
 def phase_voltage(p, v): return p[0]+p[1]*(v**2)
-def counts_phase(p, phase): return p[2]*(1-p[3]*np.sin(phase)*np.sin(phase))
+def counts_phase(p, phase): return p[2]*(1-p[3]*np.sin(phase/2)*np.sin(phase/2))
 def counts_voltage(p, voltage): return counts_phase(p, phase_voltage(p, voltage))
 def errfunc(p, voltage, count): return np.sum(np.power(counts_voltage(p, voltage)-count, 2))
 
