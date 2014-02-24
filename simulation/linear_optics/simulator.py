59dc940911d872b378b3dda66562896066d69a00
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-02-24 20:06:00 +0000

    progress bars

--- simulation/linear_optics/simulator.py
+++ simulation/linear_optics/simulator.py
@@ -4,6 +4,7 @@ from qy.simulation import permanent
 from qy import util
 from state import state
 from basis import basis
+import progressbar as pb
 
 class simulator:
     ''' Get states and statistics from a device '''
@@ -46,6 +47,7 @@ class simulator:
     def get_output_state(self):
         ''' Get the output state, assuming indistinguishable photons'''
         output_state=self.basis.get_state()
+        pbar = pb.ProgressBar(widgets=[pb.Percentage()], maxval=self.basis.hilbert_space_dimension).start()
         for input in self.input_state.nonzero_terms:
             input_amplitude=self.input_state.vector[input]
             cols=self.basis.modes_from_index(input)
@@ -54,13 +56,15 @@ class simulator:
                 rows=self.basis.modes_from_index(output)
                 n2=self.basis.get_normalization_constant(rows)
                 output_state[output]+=input_amplitude*self.perm(self.device.unitary[rows][:,cols])/np.sqrt(n1*n2)
-                util.progress_bar(output, self.basis.hilbert_space_dimension, label='Computing output state...')
+                pbar.update(output)
+        pbar.finish()
         return output_state
 
     def get_probabilities_quantum(self, outputs=None):
         ''' Iterate over a bunch of patterns.  Outputs must be a list or generator of indeces '''
         N=len(outputs)
         amplitudes=np.zeros(N, dtype=complex)
+        pbar = pb.ProgressBar(widgets=[pb.Percentage()], maxval=N).start()
         for input in self.input_state.nonzero_terms:
             input_amplitude=self.input_state.vector[input]
             cols=self.basis.modes_from_index(input)
@@ -69,15 +73,17 @@ class simulator:
                 rows=self.basis.modes_from_index(output)
                 n2=self.basis.get_normalization_constant(rows)
                 amplitudes[index]+=input_amplitude*self.perm(self.device.unitary[rows][:,cols])/np.sqrt(n1*n2)
-                util.progress_bar(index, N, label='Computing probabilities...')
+                pbar.update(index)
         probabilities=np.abs(amplitudes)
         probabilities=probabilities*probabilities
+        pbar.finish()
         return probabilities
 
     def get_probabilities_classical(self, outputs=None):
         ''' Iterate over a bunch of patterns.  Outputs must be a list or generator of indeces '''
         N=len(outputs)
         probabilities=np.zeros(N, dtype=float)
+        pbar = pb.ProgressBar(widgets=[pb.Percentage()], maxval=N).start()
         for input in self.input_state.nonzero_terms:
             cols=self.basis.modes_from_index(input)
             input_amplitude=self.input_state.vector[input]
@@ -88,7 +94,8 @@ class simulator:
                 submatrix=np.abs(self.device.unitary[rows][:,cols])
                 submatrix=np.multiply(submatrix, submatrix)
                 probabilities[index]+=input_probability*self.perm(submatrix)/n2
-                util.progress_bar(index, N, label='Computing probabilities...')
+                pbar.update(index)
+        pbar.finish()
         return probabilities
 
     def get_probabilities_limited_visibility(self, outputs=None):
@@ -128,10 +135,12 @@ class simulator:
 
         # Label the list of probabilities and make sure that it is sorted
         d={}
+        pbar = pb.ProgressBar(widgets=[pb.Percentage()], maxval=len(outputs)).start()
         for index, output in enumerate(outputs):
             label=tuple(self.basis.modes_from_index(output))
             d[label]=probabilities[index]
-            util.progress_bar(index, len(outputs), label='Labelling...')
+            pbar.update(index)
+        pbar.finish()
         return util.dict_to_sorted_numpy(d)
 
     def get_probability_quantum(self, pattern):
@@ -204,13 +213,13 @@ if __name__=='__main__':
 
     print '\nBigger devices:'
     from random_unitary import random_unitary
-    device=random_unitary(3)
-    sim=simulator(device, nphotons=3)
+    device=random_unitary(20)
+    sim=simulator(device, nphotons=5)
     sim.set_input_state(range(sim.basis.nphotons))
     output_state=sim.get_output_state()
     print 'Output probabilities:'
-    print sim.get_probabilities()
-    print 'Output state:'
-    print output_state
-    print np.abs(output_state.vector)**2
+    probs= sim.get_probabilities()
+    #print 'Output state:'
+    #print output_state
+    #print np.abs(output_state.vector)**2
 
